f0c18ce04b03689f57d43fccea6afb7a4430565f
Jason Stajich <jason@bioperl.org>
2014-09-13 21:15:54 -0700

    running RIP creation

---
+++ neurospora/MSUD/circos/run_RIP.sh
@@ -0,0 +1,2 @@
+perl /rhome/jstajich/src/fungaltools/scripts/RIP_index_calculation.pl -i /shared/stajichlab/projects/neurospora_MSUD/Nc12/neurospora_crassa_or74a_12_supercontigs.fasta -t composite -r wig -o ./Nc12.RIP.wig -w 1000 -s 250 
+
---
+++ neurospora/MSUD/circos/run_RIP_long.sh
@@ -0,0 +1,2 @@
+perl /rhome/jstajich/src/fungaltools/scripts/RIP_index_calculation.pl -i /shared/stajichlab/projects/neurospora_MSUD/Nc12/neurospora_crassa_or74a_12_supercontigs.fasta -t composite -r bed -o ./Nc12.RIP_long.bed -w 5000 -s 2500 
+
